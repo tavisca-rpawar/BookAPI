d60d03222000200b202277b68a54820e787d2628
tavisca-rpawar <rpawar@tavisca.in>
2019-09-06 11:28:50 +0530

    Added Actions

--- XUnitTesT/BookServicesFixture.cs
+++ XUnitTesT/BookServicesFixture.cs
@@ -9,41 +9,50 @@ namespace XUnitTest
     public class BookServicesFixture
     {
         BookServices bookServices = new BookServices();
-        Book expected = new Book { Id = 0, Title = "The Monk Who sold his Ferrari", Author = "Robin Sharma", Category = "Fiction" };
+        Book bookList = new Book { Id = 0, Title = "The Monk Who sold his Ferrari", Author = "Robin Sharma", Category = "Fiction" };
+        Book updatedBookList = new Book { Id = 0, Title = "Revolution 2020", Author = "Chetan Bhagat", Category = "Romantic" };
+
         [Fact]
         public void GetBook_should_return_list_of_books_test()
         {
-            bookServices.AddBook(expected);
-            Assert.Contains(expected, bookServices.GetBook());
+            bookServices.AddBook(bookList);
+            var actual = bookServices.GetBook();
+            var expected = bookList;
+            Assert.Contains(expected, actual);
         }
         [Fact]
         public void GetBookById_should_return_book_with_specific_id_test()
         {
             int id = 0;
-            bookServices.AddBook(expected);
-            Book actual = bookServices.GetBookById(id);
+            bookServices.AddBook(bookList);
+            var actual = bookServices.GetBookById(id);
+            var expected = bookList;
             Assert.Equal(expected, actual);
             
         }
         [Fact]
         public void AddBook_should_add_book_in_bookData_test()
         {
-            bookServices.AddBook(expected);
-            Assert.Contains(expected, bookServices.GetBook());
+            bookServices.AddBook(bookList);
+            var actual = bookServices.GetBook();
+            var expected = bookList;
+            Assert.Contains(expected, actual);
         }
         [Fact]
         public void UpdatedBook_should_update_book_in_bookData_test()
         {
             int id = 0;
-            bookServices.AddBook(expected);
-            bookServices.UpdateBook(id, expected);
-            Assert.Contains(expected, bookServices.GetBook());
+            bookServices.AddBook(bookList);
+            bookServices.UpdateBook(id, updatedBookList);
+            var actual = bookServices.GetBook();
+            var expected = updatedBookList; 
+            Assert.Contains(expected, actual);
         }
         [Fact]
         public void DeleteBookByID_should_delete_book_from_bookData_test()
         {
             int id = 0;
-            bookServices.AddBook(expected);
+            bookServices.AddBook(bookList);
             bookServices.DeleteBookByID(id);
             Assert.Null(bookServices.GetBookById(id));
         }
